Add silder (something wrong with calculation)

main.py
@@ -4,6 +4,7 @@ import dataclasses
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FFMpegWriter
+from matplotlib.widgets import Slider
 
 
 @dataclasses.dataclass
@@ -60,13 +61,11 @@ class Simulation:
         self.particle = particle
         self.forces = forces
 
-    def simulate(self, dt: float, duration: float, ax, writer: FFMpegWriter = None, time_factor: int = 2):
+    def simulate(self, dt: float, duration: float, plot, writer: FFMpegWriter = None, time_factor: int = 2):
         iterations = int(duration / dt)
         x = np.empty([iterations])
         y = np.empty_like(x)
         t = 0
-        ax.scatter(*self.particle.position, marker='x')
-        p, = plt.plot([], [])
         for i in range(iterations):
             total_force = sum(map(lambda force: force.at(self.particle, t), self.forces))
             acceleration = total_force / self.particle.mass
@@ -75,13 +74,12 @@ class Simulation:
             x[i], y[i] = self.particle.position
             t += dt
             if writer is not None and (i % (iterations // (writer.fps * time_factor))) == 0:
-                p.set_data(x[:i], y[:i])
+                plot.set_data(x[:i], y[:i])
                 writer.grab_frame()
-        p.set_data(x, y)
-        ax.scatter(*self.particle.position, marker='x')
+        plot.set_data(x, y)
 
     def visualize_field(self, field: Force, ax, time: float = 0):
-        X = np.arange(*ax.get_xlim(), (max(ax.get_xlim()) - min(ax.get_xlim())) / 10)
+        X = np.arange(*ax.get_xlim(), (max(ax.get_xlim()) - min(ax.get_xlim())) / 10)  # TODO: Use linspace
         Y = np.arange(*ax.get_ylim(), (max(ax.get_ylim()) - min(ax.get_ylim())) / 10)
         U, V = np.meshgrid(X, Y)
         Ex, Ey = field.at(Particle(0, self.particle.charge, 0, (U, V)), 0)
@@ -102,13 +100,51 @@ def calculate_particle_mass():
 
 
 def main():
+    simulation, A2, Vac, omega = create_simulation()
+
+    fig, ax = plt.subplots()
+    # plt.title(fr"$V_{{ac}} = {Vac}, \Gamma = {gamma}, Z_{{eff}} = {Zeff}, \omega=2\pi/{Vac_frequency}$")
+
+    trap_size = 0.01
+    ax.set_xlim(-trap_size, trap_size)
+    ax.set_ylim(-trap_size, trap_size)
+
+    simulation.visualize_field(ElectricForce(A2, omega), ax)
+    plot, = plt.plot([], [])
+    record = False
+    if record:
+        metadata = dict(title='Movie', artist='codinglikemad')
+        writer = FFMpegWriter(fps=60, metadata=metadata)
+        with writer.saving(fig, 'Movie.mp4', 100):
+            simulation.simulate(0.00005, 2, plot, writer, time_factor=2)
+            writer.grab_frame()
+    else:
+        simulation.simulate(0.00005, 1, plot)
+        Vac_slider = Slider(plt.axes([0.25, 0, 0.65, 0.03]), '$V_{ac}$', 0, 6000, valinit=Vac, valstep=200)
+
+        def update_Vac(val):
+            print("A")
+            simulation, A2, Vac, omega = create_simulation(Vac=val)
+            simulation.simulate(0.00005, 1, plot)
+            print("B")
+            fig.canvas.draw_idle()
+            print("C")
+
+        Vac_slider.on_changed(update_Vac)
+
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    plt.show()
+
+
+def create_simulation(Vac: float = 3000):
     initial_position = np.array([-0.003, 0.003])  # Trap size is no the order of 1 cm (0.01 m)
     initial_velocity = np.array([-0.2, -0.1])
     pollen_mass = calculate_particle_mass()
     pollen_charge = 2e-14  # 7.359e-15 derived from q = g*m/E0, our calculation resulted in 5.11e-15
     Vac_frequency = 50.0  # 50 Hz from outlet
     omega = 2 * np.pi * Vac_frequency
-    Vac = 3000  # 0-6 kV
+    # Vac = 3000  # 0-6 kV
     gamma = 0.02 * 1620  # 1620 Hz From IonTrapPhysics pdf
     # Zeff is a constant that depends on the geometry of the trap, and we expect it's value to be comparable to the
     # spacing between the trap electrodes. We will assume 1 cm which is 0.01 m.
@@ -116,40 +152,17 @@ def main():
     A2 = (Vac / Zeff ** 2) / (-4)
     forces = [
         ElectricForce(A2, omega),
-        # DragForce(gamma),
-        # GravitationalForce(g=9.8),
-        # ElectrostaticForce(E0=5000)
+        DragForce(gamma),
+        GravitationalForce(g=9.8),
+        ElectrostaticForce(E0=5000)
     ]
     particle = Particle(pollen_mass, pollen_charge, initial_velocity, initial_position)
-
     if Vac < (particle.mass * omega * gamma * (Zeff ** 2) / particle.charge):
         print("System is stable")
     else:
         print("System is not stable")
-
-    fig, ax = plt.subplots()
-    plt.title(fr"$V_{{ac}} = {Vac}, \Gamma = {gamma}, Z_{{eff}} = {Zeff}, \omega=2\pi/{Vac_frequency}$")
-
-    trap_size = 0.01
-    ax.set_xlim(-trap_size, trap_size)
-    ax.set_ylim(-trap_size, trap_size)
-
     simulation = Simulation(particle, forces)
-    record = True
-    if record:
-        metadata = dict(title='Movie', artist='codinglikemad')
-        writer = FFMpegWriter(fps=60, metadata=metadata)
-        with writer.saving(fig, 'Movie.mp4', 100):
-            simulation.visualize_field(ElectricForce(A2, omega), ax)
-            simulation.simulate(0.00005, 2, ax, writer, time_factor=2)
-            writer.grab_frame()
-    else:
-        simulation.simulate(0.00005, 1, ax)
-
-    simulation.visualize_field(ElectricForce(A2, omega), ax)
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    plt.show()
+    return simulation, A2, Vac, omega
 
 
 if __name__ == '__main__':