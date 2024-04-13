dcf44f3e9d55b71b7326b496d8b889a9f7c34582
Steven Dashevsky <dashevskysteven@gmail.com>
2024-04-13 13:37:07 +0300

    Change drag force. Add global configuration.

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from matplotlib.animation import FFMpegWriter
 from matplotlib.widgets import Slider
 
 
+DENSITY = 510  # 510+-40 kg/m^3
+RADIUS = 26e-6 / 2.0  # The diameter 26+-2.5 um
+
+
 @dataclasses.dataclass
 class Particle:
     mass: float
@@ -33,11 +37,9 @@ class ElectricForce(Force):
 
 
 class DragForce(Force):
-    def __init__(self, gamma: float):
-        self.gamma = gamma
-
     def at(self, particle: Particle, time: float):
-        return - (self.gamma * particle.mass) * particle.velocity
+        mu = 1.8e-5  # Air dynamic viscosity in kg / (m * sec)
+        return -6 * np.pi * mu * RADIUS * particle.velocity
 
 
 class GravitationalForce(Force):
@@ -92,62 +94,74 @@ def calculate_particle_mass():
     Note: Our measurement shows 3.379e-13 kg
     :return:
     """
-    DENSITY = 510  # 510+-40 kg/m^3
-    RADIUS = 26e-6 / 2.0  # The diameter 26+-2.5 um
     volume = (4 / 3.0) * np.pi * RADIUS ** 3
     mass = DENSITY * volume
     return mass
 
+@dataclasses.dataclass
+class Configuration:
+    Vac: float = 3000  # 0-6 kV
+    Zeff: float = 1.16e-3  # Derived from our measurements
+    Vac_frequency: float = 50.0  # 50 Hz from outlet
+    g: float = 9.8
+    E0: float = 5000.
+    x0: float = -0.002
+    y0: float = 0.002
+    vx0: float = 0.
+    vy0: float = 0.
+    pollen_charge: float = 7.359e-15  # 7.359e-15 derived from q = g*m/E0, our calculation resulted in 5.11e-15
+    pollen_mass: float = calculate_particle_mass()
+
+    def __post_init__(self):
+        self.E0 = self.pollen_mass * self.g / self.pollen_charge  # Set
+
 
 def create_simulation(ax,
-                      Vac: float = 3000,  # 0-6 kV
-                      gamma: float = 0,  # 1620 Hz From IonTrapPhysics pdf
-                      Zeff: float = 0.005,
-                      Vac_frequency: float = 50.0,  # 50 Hz from outlet
-                      g: float = 9.8,
-                      E0: float = 5000.,
-                      x0: float = -0.003,
-                      y0: float = 0.003,
-                      vx0: float = -0.1,
-                      vy0: float = -0.1
+                      Vac: float,
+                      Zeff: float,
+                      Vac_frequency: float,
+                      g: float,
+                      E0: float,
+                      x0: float,
+                      y0: float,
+                      vx0: float,
+                      vy0: float,
+                      pollen_charge: float,
+                      pollen_mass: float,
 ):
     initial_position = np.array([x0, y0])
     initial_velocity = np.array([vx0, vy0])
-    pollen_mass = calculate_particle_mass()
-    pollen_charge = 2e-14  # 7.359e-15 derived from q = g*m/E0, our calculation resulted in 5.11e-15
     # Zeff is a constant that depends on the geometry of the trap, and we expect it's value to be comparable to the
     # spacing between the trap electrodes. We will assume 1 cm which is 0.01 m.
     omega = 2 * np.pi * Vac_frequency
     A2 = (Vac / Zeff ** 2) / (-4)
-    forces = [ElectricForce(A2, omega), DragForce(gamma), ElectrostaticForce(E0), GravitationalForce(g)]
+    forces = [ElectricForce(A2, omega), DragForce(), ElectrostaticForce(E0), GravitationalForce(g)]
     particle = Particle(pollen_mass, pollen_charge, initial_velocity, initial_position)
-    if Vac < (particle.mass * omega * gamma * (Zeff ** 2) / particle.charge):
-        print("System is stable")
-    else:
-        print("System is not stable")
     simulation = Simulation(particle, forces)
     simulation.visualize_field(ElectricForce(A2, omega), ax)
-    plt.suptitle(fr"$V_{{ac}} = {Vac}V, \Gamma = {gamma} Hz, Z_{{eff}} = {Zeff} m, \omega=2\pi*{Vac_frequency} Hz$")
+    plt.suptitle(fr"Paul trap simulation")
     return simulation
 
 
 def main():
-    fig = plt.figure(figsize=(6, 6))
+    fig = plt.figure(figsize=(6, 7))
     ax = fig.add_axes([0.2, 0.3, 0.75, 0.60])
-    trap_size = 0.02
-    ax.set_xlim(-trap_size, trap_size)
-    ax.set_ylim(-trap_size, trap_size)
+    trap_size = 0.01
+    ax.set_xlim(-trap_size*2, trap_size*2)
+    ax.set_ylim(-trap_size*2, trap_size*2)
     Vac_axes = fig.add_axes([0.25, 0, 0.65, 0.03])
     Zeff_axes = fig.add_axes([0.25, 0.03, 0.65, 0.03])
-    gamam_axes = fig.add_axes([0.25, 0.06, 0.65, 0.03])
+    q_axes = fig.add_axes([0.25, 0.06, 0.65, 0.03])
     frequency_axes = fig.add_axes([0.25, 0.09, 0.65, 0.03])
     g_axes = fig.add_axes([0.25, 0.12, 0.65, 0.03])
     E0_axes = fig.add_axes([0.25, 0.15, 0.65, 0.03])
-    vx0_axes = fig.add_axes([0.25, 0.21, 0.25, 0.03])
-    vy0_axes = fig.add_axes([0.65, 0.21, 0.25, 0.03])
     x0_axes = fig.add_axes([0.25, 0.18, 0.25, 0.03])
     y0_axes = fig.add_axes([0.65, 0.18, 0.25, 0.03])
-    simulation = create_simulation(ax)
+    vx0_axes = fig.add_axes([0.25, 0.21, 0.25, 0.03])
+    vy0_axes = fig.add_axes([0.65, 0.21, 0.25, 0.03])
+
+    configuration = Configuration()
+    simulation = create_simulation(ax, **configuration.__dict__)
 
     plot, = ax.plot([], [])
     record = False
@@ -159,27 +173,25 @@ def main():
             writer.grab_frame()
     else:
         simulation.simulate(0.00005, 1, plot)
-        Vac_slider = Slider(Vac_axes, '$V_{ac}$', 0, 6000, valinit=3000, valstep=200)
-        Zeff_slider = Slider(Zeff_axes, '$Z_{eff}$', 0, 0.02, valinit=0.01 * 0.5, valstep=0.001)
-        gamma_slider = Slider(gamam_axes, r'$\Gamma$', 0, 2000, valinit=0, valstep=10)
-        frequency_slider = Slider(frequency_axes, r'$V_{ac} frequency$', 0, 100, valinit=50, valstep=10)
-        g_slider = Slider(g_axes, r'$g$', 0, 15, valinit=9.8, valstep=0.1)
-        E0_slider = Slider(E0_axes, r'$E_0$', 0, 10000, valinit=5000, valstep=1000)
-        x0_slider = Slider(x0_axes, r'$x_0$', -0.01, 0.01, valinit=-0.003, valstep=0.001)
-        y0_slider = Slider(y0_axes, r'$y_0$', -0.01, 0.01, valinit=0.003, valstep=0.001)
-        vx0_slider = Slider(vx0_axes, r'$vx_0$', -0.5, 0.5, valinit=-0.01, valstep=0.001)
-        vy0_slider = Slider(vy0_axes, r'$vy_0$', -0.5, 0.5, valinit=-0.01, valstep=0.001)
-        configuration = dict()
+        Vac_slider = Slider(Vac_axes, '$V_{ac}$', 0, configuration.Vac * 2, valinit=configuration.Vac, valstep=configuration.Vac / 10)
+        Zeff_slider = Slider(Zeff_axes, '$Z_{eff}$', 0, trap_size / 2, valinit=configuration.Zeff, valstep=0.0002)
+        frequency_slider = Slider(frequency_axes, r'$V_{ac} frequency$', 0, configuration.Vac_frequency * 2, valinit=configuration.Vac_frequency, valstep=10)
+        g_slider = Slider(g_axes, r'$g$', 0, configuration.g * 2, valinit=configuration.g, valstep=0.1)
+        E0_slider = Slider(E0_axes, r'$E_0$', 0, configuration.E0 * 2, valinit=configuration.E0, valstep=100)
+        x0_slider = Slider(x0_axes, r'$x_0$', -trap_size, trap_size, valinit=configuration.x0, valstep=trap_size / 5)
+        y0_slider = Slider(y0_axes, r'$y_0$', -trap_size, trap_size, valinit=configuration.y0, valstep=trap_size / 5)
+        vx0_slider = Slider(vx0_axes, r'$vx_0$', -trap_size * 10, trap_size * 10, valinit=configuration.vx0, valstep=trap_size)
+        vy0_slider = Slider(vy0_axes, r'$vy_0$', -trap_size * 10, trap_size * 10, valinit=configuration.vy0, valstep=trap_size)
+        q_slider = Slider(q_axes, r'Particle charge', 1e-15, 1e-14, valinit=configuration.pollen_charge, valstep=5e-16)
 
         def update(name: str, value):
-            configuration[name] = value
-            simulation = create_simulation(ax, **configuration)
-            simulation.simulate(0.00005, 1, plot)
+            setattr(configuration, name, value)
+            simulation = create_simulation(ax, **configuration.__dict__)
+            simulation.simulate(0.00005, 2, plot)
             fig.canvas.draw_idle()
 
         Vac_slider.on_changed(lambda val: update("Vac", val))
         Zeff_slider.on_changed(lambda val: update("Zeff", val))
-        gamma_slider.on_changed(lambda val: update("gamma", val))
         frequency_slider.on_changed(lambda val: update("Vac_frequency", val))
         g_slider.on_changed(lambda val: update("g", val))
         E0_slider.on_changed(lambda val: update("E0", val))
@@ -187,6 +199,7 @@ def main():
         y0_slider.on_changed(lambda val: update("y0", val))
         vx0_slider.on_changed(lambda val: update("vx0", val))
         vy0_slider.on_changed(lambda val: update("vy0", val))
+        q_slider.on_changed(lambda val: update("pollen_charge", val))
 
     ax.set_xlabel("x")
     ax.set_ylabel("y")
